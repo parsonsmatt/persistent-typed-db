deae10bfae2adbdec593f33a5b5507c31f8cd9a2
Matt Parsons <parsonsmatt@gmail.com>
2018-01-19 11:23:54 -0700

    heck yeah

---
+++ test/EsqueletoSpec.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module EsqueletoSpec where
+
+import Database.Persist.Typed
+import Database.Esqueleto
+import Database.Persist.TH
+import Test.Hspec
+
+data TestDb
+
+share [mkPersist (mkSqlSettingsFor ''TestDb)] [persistLowerCase|
+
+Person
+    name String
+    age Int
+    deriving Show Eq
+
+Dog
+    name String
+    owner PersonId
+    deriving Show Eq
+
+    |]
+
+spec :: Spec
+spec = do
+    let typeChecks = True `shouldBe` True
+    describe "select" $
+        it "type checks" $ do
+            let q :: SqlPersistMFor TestDb [(Entity Person, Entity Dog)]
+                q = select $
+                    from $ \(p `InnerJoin` d) -> do
+                    on (p ^. PersonId ==. d ^. DogOwner)
+                    pure (p, d)
+            typeChecks
+
+    describe "update" $
+        it "type checks" $ do
+            let q :: SqlPersistMFor TestDb ()
+                q = update $ \p -> do
+                    set p [ PersonName =. val "world" ]
+                    where_ (p ^. PersonName ==. val "hello")
+            typeChecks
+
+    describe "delete" $
+        it "type checks" $ do
+            let q :: SqlPersistMFor TestDb ()
+                q = delete $ from $ \p -> where_ (p ^. PersonName ==. val "world")
+            typeChecks
