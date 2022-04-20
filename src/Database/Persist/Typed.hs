e1cd51b38425dceaa0605df2ea30cb727c708f8d
Curran McConnell <curran@mercury.com>
2022-04-20 12:00:45 -0400

    fix some warnings

--- src/Database/Persist/Typed.hs
+++ src/Database/Persist/Typed.hs
@@ -50,9 +50,8 @@ import qualified Data.Foldable as Foldable
 import Data.Foldable (toList)
 import Data.Int (Int64)
 import Data.List (find, inits, transpose)
-import qualified Data.List.NonEmpty as NEL
 import Data.Maybe (isJust)
-import Data.Monoid (mappend, (<>))
+import Data.Monoid (mappend)
 import Data.Pool (Pool)
 import Data.Text (Text)
 import qualified Data.Text as Text
@@ -525,12 +524,12 @@ instance PersistUniqueWrite (SqlFor db) where
       conn <- ask
       let escape = connEscapeRawName conn
       let refCol n = Text.concat [escape (unEntityNameDB $ getEntityDBName t), ".", n]
-      let mkUpdateText = mkUpdateText' (escape . unFieldNameDB) refCol
+      let mkUpdateFieldText = mkUpdateText' (escape . unFieldNameDB) refCol
       case connUpsertSql conn of
         Just upsertSql -> case updates of
                             [] -> generalizeQuery $ defaultUpsertBy uniqueKey record updates
                             _:_ -> do
-                                let upds = Text.intercalate "," $ map mkUpdateText updates
+                                let upds = Text.intercalate "," $ map mkUpdateFieldText updates
                                     sql = upsertSql t (persistUniqueToFieldNames uniqueKey) upds
                                     vals = map toPersistValue (toPersistFields record)
                                         ++ map updatePersistValue updates
